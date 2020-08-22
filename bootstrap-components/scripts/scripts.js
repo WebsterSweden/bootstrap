51e11080d54ae338931a81391d03741068178c60
WebsterSweden <webster@webster.se>
2020-08-22 12:50:02 +0200

    Create Bootstrap Components Files.

---
+++ bootstrap-components/scripts/scripts.js
@@ -0,0 +1,12 @@
+/*jslint devel, browser */
+/*global
+window
+*/
+
+// Check if the JavaScript file is linked and active.
+function filename() {
+    "use strict";
+    console.log(document.currentScript);
+}
+
+filename();
